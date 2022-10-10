b23546be4e8a0ad6f70e54df52f289af4419e5ac
Zafir <55599023+zafir100100@users.noreply.github.com>
2022-10-10 14:37:36 +0600

    code changed

--- src/javaapplication6/ServerThread.java
+++ src/javaapplication6/ServerThread.java
@@ -1,4 +1,4 @@
-package workerpattern4;
+package javaapplication6;
 
 import java.io.BufferedReader;
 import java.io.IOException;
--- src/javaapplication6/WorkerPatternToWriteFiles2.java
+++ src/javaapplication6/WorkerPatternToWriteFiles2.java
@@ -1,46 +1,39 @@
-package workerpattern4;
+package javaapplication6;
 
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.List;
-import model.Worker;
-import repository.WorkerRepository;
 
-public class Main
+public class WorkerPatternToWriteFiles2
 {
 
     static final int PORT = 1978;
 
     public static void main(String[] args)
     {
-        Socket socket = null;
-        ServerSocket ServerSocket = null;
+        Socket s = null;
+        ServerSocket ss2 = null;
         System.out.println("Server Listening......");
         try
         {
-            ServerSocket = new ServerSocket(PORT);
+            ss2 = new ServerSocket(4445); // can also use static final PORT_NUM , when defined
+
         }
         catch (IOException e)
         {
             e.printStackTrace();
             System.out.println("Server error");
+
         }
 
         while (true)
         {
             try
             {
-//                WorkerRepository wrrp = new WorkerRepository();
-//                List<Worker> l = wrrp.getWorkers();
-//
-//                Worker f = wrrp.getWorker();
-//                System.out.println(f.getName());
-                socket = ServerSocket.accept();
+                s = ss2.accept();
                 System.out.println("connection Established");
-                ServerThread serverThread = new ServerThread(socket);
-                serverThread.start();
+                ServerThread st = new ServerThread(s);
+                st.start();
 
             }
 
